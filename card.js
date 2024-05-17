6a93d829dd2ceab326455a359d743293435550e8
Tyler <penguincavalry@gmail.com>
2024-05-17 11:13:50 -0600

    Reboot of the site, think this'll go up.

---
+++ card.js
@@ -0,0 +1,64 @@
+//this file is intended to generate the HTML elements for the cards that will be displayed on the main page.  it should iterate through the recipe.js
+//file to get the text and images for each recipe.  it should also include the event listeners for the icons on the bottom bar of each card.
+//the ingredients are a nested object in the recipe object.  they have to be handled differently than the other sections.
+
+function showRecipeSection(sectionName, index, recipe = mainDish) {
+  const displayChange = document.querySelector('.cardContainer').children[index].querySelector('.textInformation');
+  const sections = ['ingredients', 'instructions', 'servingInfo'];
+  const isCurrentlyVisible = displayChange.classList.contains('showText');
+  // Update content based on visibility
+  
+  if (sectionName === displayChange.dataset.currentSection && isCurrentlyVisible) {
+    // if its the same section and already visibile, hide it.
+    console.log(`isCurrentlyVisible: ${isCurrentlyVisible}`)
+    console.log(`sectionName: ${sectionName}, index: ${index}`)
+    displayChange.classList.toggle('showText');
+  }
+  // if it's the igredients button, it needs to itterate through the nexted ingredients object.  
+  else if (sectionName === 'ingredients') {
+    // Handle ingredients section specifically
+    console.log(`sectionName: ${sectionName}, index: ${index}`)
+    console.log(`else if ${displayChange.classList}`)
+    displayChange.innerHTML = recipe[index].ingredients.map(ingredient => `${ingredient.amount} : ${ingredient.ingredient}<br>`).join('');
+    displayChange.dataset.currentSection = sectionName;
+    displayChange.classList.remove(...sections.filter(section => section !== sectionName));
+    displayChange.classList.add('showText');
+  }
+  
+  else{
+    console.log(`sectionName: ${sectionName}, index: ${index}`)
+    console.log(`displayChange: ${displayChange}.dataset.currentSection: ${displayChange.dataset.currentSection}`)
+    displayChange.classList.remove(...sections.filter(section => section !== sectionName));
+    displayChange.innerHTML = mainDish[index][sectionName];
+    displayChange.dataset.currentSection = sectionName;
+    displayChange.classList.add('showText');
+  }
+}
+const cardContainer = document.querySelector('.cardContainer');
+function createCard(recipe){
+    for(let i =0; i < recipe.length; i++){
+        const newCard = document.createElement('div');
+        newCard.classList.add('card');
+        newCard.innerHTML = `
+        <h2 id="cardTitle1">${recipe[i].title}</h2>
+        <img src="${recipe[i].imageSrc}" alt="Recipe Image 1">
+        <p class="textInformation showText ingredients">${recipe[i].ingredients.map(ingredient => `${ingredient.amount} : ${ingredient.ingredient}<br>`).join('')}
+        </p>
+        <div class="bottomBar">
+          <i class="material-symbols-outlined ingredients" onclick="showRecipeSection('ingredients', ${i})">Checklist</i>
+          <i class="material-symbols-outlined instructions" onclick="showRecipeSection('instructions', ${i})">Turn_Right</i>
+          <i class="material-symbols-outlined servinginfo" onclick="showRecipeSection('servingInfo', ${i})">Timer</i>
+        </div>
+        <div class="cardIcon">
+          <i id="rollCard" class="material-symbols-outlined" onclick="randomRecipe(${i})">deployed_code</i>
+          <i id="lockCard" class="material-symbols-outlined" onclick="lockCard(${i})">Lock</i>
+          <i id="loveCard" class="material-symbols-outlined" onclick="favRecipe(${i})">Favorite</i>
+        </div>
+        `;
+        cardContainer.appendChild(newCard);
+    }
+}
+
+window.onload = function(){
+    createCard(mainDish);
+}
---
+++ recipes.js
@@ -0,0 +1,167 @@
+// first section is for the initial dispaly.  Will be overritten by the lock button
+// by placing the values locally, so what they had locked is there when they come back.
+let mainDish = [
+    {
+        title : 'Scrambled Eggs',
+        imageSrc:  'Assets\/ScrambledEggs.jpg',
+        description : 'How to make fluffy eggs',
+        ingredients: [
+            {amount:'2',ingredient: 'eggs'},
+            {amount: '1 tbs', ingredient: 'butter'},
+            {amount: '1/4 cup', ingredient: 'milk'}],
+        instructions: 'whisk eggs and liquid together.  liquid will evaporate and create airier eggs place a sauce pan on the burner at medium low heat.  Melt the butter.  add eggs to the pan and whisk intermetantly, removing from heat occasionally to keep from cooking too fast.  remove from heat when curds form, while they\'re still glossy',
+        servingInfo: 'Serves 2, 10 minutes to prepare, 5 minutes to cook, 500 calories per serving',
+        category:'Entree'
+    },
+{
+    title : 'Cottage Loaf',
+    imageSrc:  'Assets\/cottageLoaf.jpg',
+    description : 'Loaf of a Cottage',
+    ingredients: [
+        {amount: '150 g',
+        ingredient: 'sourdough starter'},
+        {amount: '500 g',
+        ingredient: 'bread flour'},
+        
+        {amount: '10 g',
+        ingredient: 'salt'},
+        {amount: '25 g',
+        ingredient: 'oil'
+        }
+    ],
+    instructions: 'put everything together in a mixer and mix for 5 minutes on medium speed.  let rise for 2 hours.  shape into a loaf and let rise for another hour.  bake at 450 for 30 minutes',
+    servingInfo: 'Serves 2, 10 minutes to prepare, 5 minutes to cook, 500 calories per serving',
+    category:'Side'
+},
+{
+    title : 'Grilled Avacado Carrots',
+    imageSrc:    'Assets\/GrilledCarrots.png',
+    description : 'Delightful Vegetable for Summer Grilling',
+    ingredients : [{amount: '4',
+        ingredient: 'carrots'},
+        {amount: '1',
+        ingredient: 'avacado'},
+        {amount: '10 g',
+        ingredient: 'diced mint'},
+        {amount: '1 cup',
+        ingredient: 'cup Maple Syrup'}],
+    instructions: 'rub carrots in olive oil salt and pepper to taste.  grill for 12 minutes, flipping half way.  Mix with min, maple and sliced avacado',
+    servingInfo: 'Serves 2, 10 minutes to prepare, 5 minutes to cook, 500 calories per serving',
+    category:'Vegetable'
+}
+]
+
+let recipes = [
+    {
+        title : 'Scrambled Eggs',
+        imageSrc:  'Assets\/ScrambledEggs.png',
+        description : 'How to make fluffy eggs',
+        ingredients: [
+            {amount: '2', ingredient: 'eggs'},
+            {amount: '1 tbs', ingredient: 'butter'},
+            {amount: '1/4 cup', ingredient: 'milk'}
+        ],
+        instructions: 'whisk eggs and liquid together.  liquid will evaporate and create airier eggs place a sauce pan on the burner at medium low heat.  Melt the butter.  add eggs to the pan and whisk intermetantly, removing from heat occasionally to keep from cooking too fast.  remove from heat when curds form, while they\'re still glossy',
+        category:'Entree'
+    },
+    {
+        title : 'Cottage Loaf',
+        imageSrc:  'Assets\/cottageLoaf.png',
+        description : 'Loaf of a Cottage',
+        ingredients: [
+            {amount: '150 g',ingredient: 'sourdough starter'},
+            {amount: '500 g', ingredient: 'bread flour'},
+            {amount: '10 g', ingredient: 'salt'},
+            {amount: '25 g', ingredient: 'oil'}
+        ],
+        instructions: 'put everything together in a mixer and mix for 5 minutes on medium speed.  let rise for 2 hours.  shape into a loaf and let rise for another hour.  bake at 450 for 30 minutes',
+        category:'Side'
+    },
+    {
+        title : 'Instant Pot Tater Tot Casserole',
+        imageSrc:    'Assets\/TaterTotCasserole.png',
+        description : 'Hearty American Comfort Food',
+        ingredients : '1 pound extra lean or lean ground beef or ground turkey 1 cup diced onion half tsp garlic powder quarter tsp salt        quarter tsp pepper        1 tsp Worcesteshire        three quarters cup beef or chicken broth        1 12 oz bag frozen cut green beans        1 14 oz can corn drained or Frozen        1 10 oz can cream of mushroom soup        1/4 cup sour cream       16 oz frozen tater tots half cups grated cheddar cheese',
+        instructions: 'brown beef, toss everything on top, cook',
+        category:'Entree'
+    },
+    {
+        title : 'Grilled Avacado Carrots',
+        imageSrc:    'Assets\/GrilledCarrots.png',
+        description : 'Delightful Vegetable for Summer Grilling',
+        ingredients : [{amount: '4',
+            ingredient: 'carrots'},
+            {amount: '1',
+            ingredient: 'avacado'},
+            {amount: '10 g',
+            ingredient: 'diced mint'},
+            {amount: '1 cup',
+            ingredient: 'cup Maple Syrup'}],
+        instructions: 'rub carrots in olive oil salt and pepper to taste.  grill for 12 minutes, flipping half way.  Mix with min, maple and sliced avacado',
+        category:'Vegetable'
+    },
+    {
+        title : 'Roasted Veggies',
+        imageSrc:    'Assets\/RoastedVeggies.png',
+        description : 'Easy way to make the Vegetables tastey',
+        ingredients : '1lb Broccoli 1 lb Brussle Sprouts 1 Onion 2 bell peppers 1 lb potatoes',
+        instructions: 'toss with olive oil, salt and pepper.  Place on cookie sheet covered with tinfoil.  Broil at 425 for 20-25 minutes',
+        category:'Vegetable'
+    },
+    {
+        title : 'Peanut Curry Stew',
+        imageSrc:    'Assets\/PeanutCurryStew.png',
+        description : 'Sweet and Savory delight',
+        ingredients : 'Peanut butter, Curry, Cumin, Fish Sauce, Rice, Protien of choice (Pork, Chicken, Tofu, Beef), Butter ',
+        instructions: 'Sear the meat, ',
+        category:'Entree'
+    },
+    {
+        title : 'Chicken and Waffles',
+        imageSrc:    'Assets\/ChickenAndWaffles.png',
+        description : 'O boy are you in for a treat',
+        ingredients : 'Chicken and Waffles',
+        instructions: 'Give the chicken waffles',
+        category:'Side'
+    },
+    {
+        title : 'Twice Baked Potato',
+        imageSrc:  'Assets\/LoadedBakedPotato.png',
+        description : 'All the good stuff',
+        ingredients : 'Potato Bacon Chives Cheese',
+        instructions: 'Bake it... twice?',
+        category:'Side'
+    },
+    {
+        title : 'Hot Wings',
+        imageSrc:  'Assets\/HotWings.png',
+        description : 'If it burns your house down its just right',
+        ingredients : 'Chicken, Franks Hot Sauce [Redacted]',
+        instructions: 'Make yer sauce, bread em, Fry em',
+        category:'Side'
+    },
+    {
+        title : 'Steak with Chimichuri Sauce',
+        imageSrc:  'Assets\/SteakChimChuri.png',
+        description : 'Delightful spicy topping to beef',
+        ingredients : 'Typically, its green hot peppers with vinegar and olive oil, and parsley for some bitterness and texture.  if you MUST, use Cilantro',
+        instructions: 'Make yer sauce, bread em, Fry em',
+        category:'Entree'
+    },
+    {
+        title : 'Eggplant Parmasean',
+        imageSrc:  'Assets\/EggPlantParm.png',
+        description : 'Somewhat healthier than the Chicken version',
+        ingredients : 'Eggplant Noodles Parmasean Marinara',
+        instructions: 'Cook the Eggplant till it is done',
+        category:'Vegetable'
+    },
+    {
+        title : 'Eggplant Parmasean',
+        imageSrc:  'Assets\/EggPlantParm.png',
+        description : 'Somewhat healthier than the Chicken version',
+        ingredients : 'Eggplant Noodles Parmasean Marinara',
+        instructions: 'Cook the Eggplant till it is done',
+        category:'Vegetable'
+    },
+]
