b5b2052fd63a6751fb80d52a0e1aaa93723e3a62
Tyler <penguincavalry@gmail.com>
2024-05-20 16:38:11 -0600

    A

--- recipes.js
+++ recipes.js
@@ -3,7 +3,7 @@
 let mainDish = [
     {
         title : 'Scrambled Eggs',
-        imageSrc:  'Assets\/ScrambledEggs.jpg',
+        imageSrc:  './Assets\/ScrambledEggs.jpg',
         description : 'How to make fluffy eggs',
         ingredients: [
             {amount:'2',ingredient: 'eggs'},
@@ -15,7 +15,7 @@ let mainDish = [
     },
 {
     title : 'Cottage Loaf',
-    imageSrc:  'Assets\/cottageLoaf.jpg',
+    imageSrc:  './Assets\/cottageLoaf.jpg',
     description : 'Loaf of a Cottage',
     ingredients: [
         {amount: '150 g',
@@ -35,7 +35,7 @@ let mainDish = [
 },
 {
     title : 'Grilled Avacado Carrots',
-    imageSrc:    'Assets\/GrilledCarrots.png',
+    imageSrc:    './Assets\/GrilledCarrots.png',
     description : 'Delightful Vegetable for Summer Grilling',
     ingredients : [{amount: '4',
         ingredient: 'carrots'},
